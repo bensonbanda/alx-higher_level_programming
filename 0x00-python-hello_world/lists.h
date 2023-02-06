57893b1ac669d23a35e71d87943d267a2d209dd3
bensonbanda <bbanda79@yahoo.com>
2023-02-06 03:20:10 -0800

    add file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,18 +1,17 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stio.h>
+#include <stdlib.h>
 
 typedef struct listint_s
 {
-	int num;
+	int n;
 	struct listint_s *next;
 }listint_t;
 
-size_t print_listint(cost listint_t *h);
-listint_t *add_nodeint(listint_t **head, cost int num);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
-int list_cycle(listint *list);
-
-#endif
+#endif /* LISTS_H */
