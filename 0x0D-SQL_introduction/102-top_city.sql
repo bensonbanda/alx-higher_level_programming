c34b715a863e0450fadb906705a6623f581e78bd
bensonbanda <bbanda79@yahoo.com>
2023-02-14 12:28:46 -0800

    add files

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Displays the cities with average temperatures
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperature`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
